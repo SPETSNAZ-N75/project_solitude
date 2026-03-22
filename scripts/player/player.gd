714e24aa948bdda2e911f5afe6ee16e9d39cb7d9
SPETSNAZ <59780828+SPETSNAZ-N75@users.noreply.github.com>
2026-03-22 19:26:55 +0530

    .

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	
 	# set input start
 	var move_x = Input.get_axis("move_left","move_right")
